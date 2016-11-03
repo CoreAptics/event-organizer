896e9cf8d969757ca0e4389465d505eb228dcfb8
AlexeiCreney <alexei.taupiot@gmail.com>
2016-11-03 12:28:37 +0100

    Add CoreBundle + FrontBundle + BackBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new CoreBundle\CoreBundle(),
+            new BackBundle\BackBundle(),
+            new FrontBundle\FrontBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
